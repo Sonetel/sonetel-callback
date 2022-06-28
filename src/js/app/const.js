754f559d7a8bdf4a46ef50951e6deba9a60df2d4
Aashish <aashish@aashishjoshi.co.in>
2022-06-28 14:04:08 +0530

    move constants to a separate file

---
+++ src/js/app/const.js
@@ -0,0 +1,19 @@
+
+const APP_VERSION = '1.0.0';
+
+// API URIs
+const API_BASE = 'https://public-api.sonetel.com';
+const AUTH_API = 'https://api.sonetel.com/SonetelAuth/beta/oauth/token';
+const CALLBACK_URI = '/make-calls/call/call-back';
+
+// Element IDs
+const MSG_ID = 'messageBox';
+const CALL1_ID = 'call1';
+const CALL1_SETTINGS_ID = 'call1Settings';
+const CALL2_ID = 'call2';
+const SIGNIN_FORM_ID = 'signin';
+const MAKECALL_FORM_ID = "makecall";
+const LOGOUT_BTN_ID = "logoutButton";
+
+// Messages
+const CALL_SUCCESS_MSG = "<p><strong>Success</strong>. Your call will be connected soon.</p>";
