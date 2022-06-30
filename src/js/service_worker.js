8e82b015caca549e63d10563abd77b084f7efef5
Aashish <aashish@aashishjoshi.co.in>
2022-07-01 05:22:09 +0530

    caching

--- src/js/service_worker.js
+++ src/js/service_worker.js
@@ -9,21 +9,39 @@ const assets = [
   "/assets/css/sonetel.css",
   "/assets/css/sonetel.css.map",
   "https://fonts.googleapis.com/css?family=Noto+Sans:wght@300;400,500&display=swap",
+  "https://fonts.gstatic.com/s/notosans/v27/o-0NIpQlx3QUlC5A4PNjFhdVZNyB.woff2",
+  "https://fonts.gstatic.com/s/notosans/v27/o-0NIpQlx3QUlC5A4PNjFhdVatyB1Wk.woff2",
+  "https://fonts.gstatic.com/s/notosans/v27/o-0IIpQlx3QUlC5A4PNr5TRA.woff2",
+  "https://fonts.gstatic.com/s/notosans/v27/o-0IIpQlx3QUlC5A4PNr6zRAW_0.woff2",
   "/images/logo.png",
-  "/images/info.png",
+  "/images/info.png"
 ];
 
-self.addEventListener("install", (installEvent) => {
+// Install the service worker
+self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticSonetelPwa).then((cache) => {
+    caches.open(staticSonetelPwa).then(cache => {
+      console.log('caching assets');
       cache.addAll(assets);
     })
   );
 });
 
+// Activate event
+self.addEventListener("activate", activateEvent => {
+  activateEvent.waitUntil(
+    caches.keys().then(keys => {
+      console.log(keys);
+    })
+  );
+
+});
+
+
+// fetch event
 self.addEventListener("fetch", (fetchEvent) => {
   fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then((res) => {
+    caches.match(fetchEvent.request).then(res => {
       return res || fetch(fetchEvent.request);
     })
   );
